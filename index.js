5b315234ce877036667937e5f5ef27cf6f264863
asynchronous-acrobat-1440 <lynn.x.liu@gmail.com>
2019-11-15 02:42:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,3 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string){
-  if string.toLowerCase===string
-  return "I can\'t hear you!"
-}
