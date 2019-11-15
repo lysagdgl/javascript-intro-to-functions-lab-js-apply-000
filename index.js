fc91fa7382c2b3a1aff05429d84d211d0114bb1f
asynchronous-acrobat-1440 <lynn.x.liu@gmail.com>
2019-11-15 03:00:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function sayHiToGrandma(string){
     return ("I can\'t hear you!");
     }
   else if(string.toUpperCase() === string){
-    return ("YES INDEED")
+    return ("YES INDEED!")
     }
   }
